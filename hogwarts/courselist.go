052fe06855e7b39870618161472493d3e265fa3a
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-03-31 20:16:59 +0300

    hogwarts: complete task

--- hogwarts/courselist.go
+++ hogwarts/courselist.go
@@ -3,5 +3,30 @@
 package hogwarts
 
 func GetCourseList(prereqs map[string][]string) []string {
-	return []string{}
+	result := make([]string, 0)
+	result_map := make(map[string]bool, 0)
+
+	for k := range prereqs {
+		dfs(&prereqs, &result_map, &result, k)
+	}
+
+	return result
+}
+
+func dfs(prereqs *map[string][]string, result_map *map[string]bool, result *[]string, vertex string) {
+	in_result, entered := (*result_map)[vertex]
+	if in_result {
+		return
+	}
+	if entered {
+		panic("cyclical dependence")
+	}
+	(*result_map)[vertex] = false
+
+	for _, v := range (*prereqs)[vertex] {
+		dfs(prereqs, result_map, result, v)
+	}
+
+	(*result_map)[vertex] = true
+	*result = append(*result, vertex)
 }
