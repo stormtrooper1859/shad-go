5bc90ecf5d8fe51383b527fb0bb502a57d4f6885
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-04-20 12:13:06 +0300

    urlfetch: complete

--- urlfetch/main.go
+++ urlfetch/main.go
@@ -2,6 +2,30 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	urls := os.Args[1:]
+
+	for _, url := range urls {
+		resp, err := http.Get(url)
+		check(err)
+
+		s, err := ioutil.ReadAll(resp.Body)
+		check(err)
+
+		fmt.Println(string(s))
+	}
 }
