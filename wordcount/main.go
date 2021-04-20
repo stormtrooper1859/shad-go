a2cdc2a8a9b5b0f517aeaaeced1208f19b28fe58
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-04-20 12:06:58 +0300

    complete two simple tasks

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,6 +2,30 @@
 
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
+func main() {
+	fileNames := os.Args[1:]
+
+	linesCounter := map[string]int{}
+
+	for _, fileName := range fileNames {
+		fileData, _ := ioutil.ReadFile(fileName)
+		fileLines := strings.Split(string(fileData), "\n")
+
+		for _, line := range fileLines {
+			linesCounter[line] += 1
+		}
+	}
+
+	for key, value := range linesCounter {
+		if value > 1 {
+			fmt.Printf("%d\t%s\n", value, key)
+		}
+	}
 }
