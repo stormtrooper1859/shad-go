b8ac3a63a5173b1c5263a7129dd27d75a565fcef
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-04-28 14:46:41 +0300

    utf8: complete all two subtasks

--- utf8/reverse/reverse.go
+++ utf8/reverse/reverse.go
@@ -2,6 +2,15 @@
 
 package reverse
 
+import "unicode/utf8"
+
 func Reverse(input string) string {
-	return ""
+	n := utf8.RuneCountInString(input)
+	runes := make([]rune, n)
+	i := 0
+	for _, v := range input {
+		runes[n-i-1] = v
+		i++
+	}
+	return string(runes)
 }
--- utf8/spacecollapse/collapse.go
+++ utf8/spacecollapse/collapse.go
@@ -2,6 +2,22 @@
 
 package spacecollapse
 
+import "unicode"
+
 func CollapseSpaces(input string) string {
-	return ""
+	runes := make([]rune, 0)
+	shouldSkip := false
+	for _, v := range input {
+		if unicode.IsSpace(v) {
+			if shouldSkip {
+				continue
+			}
+			shouldSkip = true
+			runes = append(runes, ' ')
+		} else {
+			shouldSkip = false
+			runes = append(runes, v)
+		}
+	}
+	return string(runes)
 }
