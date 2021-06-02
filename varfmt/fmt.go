044f256f95ac1f12167790ec5573898e3fa3b970
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-06-02 18:50:56 +0300

    varfmt: complete

--- varfmt/fmt.go
+++ varfmt/fmt.go
@@ -2,6 +2,42 @@
 
 package varfmt
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func Sprintf(format string, args ...interface{}) string {
-	return ""
+	sb := strings.Builder{}
+	stringArgs := make([]string, len(args))
+
+	prev := -1
+	globalPatternIndex := 0
+
+	for i, r := range format {
+		if r == '{' {
+			prev = i
+		}
+		if prev != -1 {
+			if r == '}' {
+				currentPatterIndex := globalPatternIndex
+				if prev+1 != i {
+					currentPatterIndex, _ = strconv.Atoi(format[prev+1 : i])
+				}
+
+				if stringArgs[currentPatterIndex] == "" {
+					stringArgs[currentPatterIndex] = fmt.Sprintf("%v", args[currentPatterIndex])
+				}
+				sb.WriteString(stringArgs[currentPatterIndex])
+
+				prev = -1
+				globalPatternIndex += 1
+			}
+		} else {
+			sb.WriteRune(r)
+		}
+	}
+
+	return sb.String()
 }
