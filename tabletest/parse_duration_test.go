9c27d6775af0ed3511244ca18e24556754900294
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-06-26 12:26:07 +0300

    tabletest: reorder tests

--- tabletest/parse_duration_test.go
+++ tabletest/parse_duration_test.go
@@ -13,24 +13,25 @@ func TestAll(t *testing.T) {
 		result    time.Duration
 		withError bool
 	}{
-		{"12m 32s", 0 * time.Second, true},
-		{"9999999999999999999999ns", 0 * time.Second, true},
-		{"9223372036854775807ns", 9223372036854775807 * time.Nanosecond, false},
-		{"9223372036854775809ns", 0 * time.Second, true},
-		{".9999999999999999999999h", 1 * time.Hour, false},
-		{".9223372036854775809h", 3320413933267 * time.Nanosecond, false},
 		{"12m32s", 752 * time.Second, false},
 		{"1m1000s", 1060 * time.Second, false},
 		{"-2m3s", -123 * time.Second, false},
 		{"0h", 0 * time.Second, false},
 		{"0", 0 * time.Second, false},
-		{"", 0 * time.Second, true},
 		{"+1.2h", 72 * time.Minute, false},
 		{"-1.2h", -72 * time.Minute, false},
+		{"1.2us", 1200 * time.Nanosecond, false},
+		{"9223372036854775807ns", 9223372036854775807 * time.Nanosecond, false},
+		{".9999999999999999999999h", 1 * time.Hour, false},
+		{".9223372036854775809h", 3320413933267 * time.Nanosecond, false},
+
+
+		{"", 0 * time.Second, true},
+		{"12m 32s", 0 * time.Second, true},
+		{"9999999999999999999999ns", 0 * time.Second, true},
+		{"9223372036854775809ns", 0 * time.Second, true},
 		{"1.2h.m", 0 * time.Second, true},
 		{"1.2.2.2h", 0 * time.Second, true},
-		{"1.2us", 1200 * time.Nanosecond, false},
-		{"-1.2us", -1200 * time.Nanosecond, false},
 		{"kek", 0 * time.Nanosecond, true},
 	}
 
