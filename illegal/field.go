f45db4de31c36c71b48637f3f1946f4040f6ab40
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-07-12 21:33:51 +0300

    illegal: complete SetPrivateField

--- illegal/field.go
+++ illegal/field.go
@@ -2,5 +2,17 @@
 
 package illegal
 
+import (
+	"reflect"
+	"unsafe"
+)
+
 func SetPrivateField(obj interface{}, name string, value interface{}) {
+	reflObj := reflect.ValueOf(obj)
+	reflValue := reflect.ValueOf(value)
+	reflField, _ := reflObj.Type().Elem().FieldByName(name)
+	fieldPointer := (unsafe.Pointer)(reflObj.Pointer() + reflField.Offset)
+
+	r := reflect.NewAt(reflValue.Type(), fieldPointer)
+	r.Elem().Set(reflValue)
 }
