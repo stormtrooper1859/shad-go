252fb84dbe4f98b99356a2222dbb081762f73c15
Aleksandr Osipov <sashaosipov.98@gmail.com>
2021-07-12 17:15:47 +0300

    reversemap: complete

--- reversemap/reversemap.go
+++ reversemap/reversemap.go
@@ -2,6 +2,24 @@
 
 package reversemap
 
+import (
+	"reflect"
+)
+
 func ReverseMap(forward interface{}) interface{} {
-	panic("implement me")
+	v := reflect.ValueOf(forward)
+	if v.Kind() != reflect.Map {
+		panic("not a map")
+	}
+
+	keyType := v.Type().Key()
+	valueType := v.Type().Elem()
+
+	result := reflect.MakeMap(reflect.MapOf(valueType, keyType))
+	for _, key := range v.MapKeys() {
+		value := v.MapIndex(key)
+		result.SetMapIndex(value, key)
+	}
+
+	return result.Interface()
 }
